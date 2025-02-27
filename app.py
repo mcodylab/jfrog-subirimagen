c2b6bf5b99994f4a270ad742d5e2582b526ac682
Milton Yarleque <milton.yarleque@gmail.com>
2025-02-27 10:34:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello Cloud Native!"}
+def hello_world():
+    return {"message": "Hello, World!"}
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
